4a02b438e704b707befd785e7f1500021a84b27d
Maksim Pegov <maksim.peg@gmail.com>
2023-09-04 16:05:16 +0200

    Font, colors and backround integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-    <App />
-);
-
+root.render(<App />);
